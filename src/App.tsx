6b35b4ca8664575ac61180f2cf5d36d47a183cf0
VItor Coke <cokevitor@gmail.com>
2022-06-28 17:31:45 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { BrowserRouter } from 'react-router-dom';
-import AppRoute from './routes/Routes';
-import LateralMenu from './shared/components/Lateral-Menu/LateralMenu';
-import DeleteProvider from './shared/contexts/DeleteContext';
-import DrawerProvider from './shared/contexts/DrawerContext';
-import AppThemeProvider from './shared/contexts/ThemeContext';
+import { BrowserRouter } from "react-router-dom";
+import AppRoute from "./routes/Routes";
+import LateralMenu from "./shared/components/Lateral-Menu/LateralMenu";
+import DeleteProvider from "./shared/contexts/DeleteContext";
+import DrawerProvider from "./shared/contexts/DrawerContext";
+import AppThemeProvider from "./shared/contexts/ThemeContext";
+import { LicenseInfo } from "@mui/x-license-pro";
+
+LicenseInfo.setLicenseKey("YOUR_LICENSE_KEY");
 
 const App = () => {
   return (
